6fb67ed1c585a05275adbacb8f938b45bf58f0f6
hollownicky <70936757+hollownicky@users.noreply.github.com>
2022-04-26 10:12:27 +0800

    Create index.js

---
+++ config/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mysql: {
+    port: 3306,
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'ni1314520',
+    database: 'spider'
+  },
+  port: 3000,
+  host: '127.0.0.1',
+  expiresIn: '2d',
+  secretKey: 'secretKey'
+}
